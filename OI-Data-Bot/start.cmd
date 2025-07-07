91fe0e237179b20360a438a5b90c91c693fa3bb5
OI-liyifan202201 <liyifan202201@outlook.com>
2025-07-07 14:59:54 +0800

    Add files via upload

---
+++ OI-Data-Bot/start.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+if exist "./OI-Data-Bot-main/main.py" goto skip
+
+echo Downloading OI-Data-Bot-main
+powershell -Command "Start-BitsTransfer -Source 'https://j.1win.ggff.net/https://github.com/OI-liyifan202201/OI-Data-Bot/archive/refs/heads/main.zip' -Destination OI.zip"
+
+echo Done
+echo Unzipping
+powershell -Command "Expand-Archive -Path OI.zip -DestinationPath ./"
+echo Done
+
+:skip
+
+pip install ttkbootstrap -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install openai -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install openai -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+python ./OI-Data-Bot-main/main.py
+
