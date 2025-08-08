768c3ae6f9514456c726a59b49867eedeee80bb3
OI-liyifan202201 <liyifan202201@outlook.com>
2025-08-08 19:48:18 +0800

    Update start.cmd

--- PCL2-CE/start.cmd
+++ PCL2-CE/start.cmd
@@ -1,19 +1,20 @@
 @echo off
-
+chcp 65001
 if exist "./PCL2/Plain Craft Launcher.exe" goto skip
-
-echo Downloading PCL2
-powershell -Command "Start-BitsTransfer -Source 'https://git.ppp.ac.cn/https://raw.githubusercontent.com/OI-liyifan202201/nbsmc-PCL2-in-GSML/refs/heads/main/nbsmc.zip' -Destination nbsmc.zip"
-
-echo Done
-echo Unzipping
+echo ========================================
+echo 正在下载 PCL2 启动器
+echo ========================================
+powershell -Command "Start-BitsTransfer -Source 'https://git.ppp.ac.cn/https  ://raw.githubusercontent.com/OI-liyifan202201/nbsmc-PCL2-in-GSML/refs/heads/main/nbsmc.zip' -Destination nbsmc.zip"
+echo 下载完成！
+echo ========================================
+echo 正在解压文件
+echo ========================================
 powershell -Command "Expand-Archive -Path nbsmc.zip -DestinationPath ./PCL2"
-echo Done
-
+echo 解压完成！
 :skip
-
+echo ========================================
+echo 启动 PCL2 启动器
+echo ========================================
 start "" "./PCL2/Plain Craft Launcher.exe"
-
-
 ping 127.0.0.1 -n 3 >nul
 exit
