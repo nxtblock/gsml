fcbcf4bd6e30a92780ba178fff727d7e080015ec
OI-liyifan202201 <liyifan202201@outlook.com>
2025-08-19 16:31:43 +0800

    Create start.cmd

---
+++ about/start.cmd
@@ -0,0 +1,12 @@
+@echo off
+chcp 65001
+echo 欢迎查看 GenGen ToolBox 关于
+echo 最新版本：
+echo GenGen ToolBox 1.3 （2025/8/19）
+echo GSML in github （2025/8/19）
+echo ---
+echo 官网 http://nxtblock.github.io
+echo GenGen ToolBox 的 github 仓库地址：https://github.com/nxtblock/ggtoolbox
+echo gsml 的 github 仓库地址：https://github.com/nxtblock/gsml
+pause
+exit
