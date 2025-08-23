ecddf199624cabefb8bafd0ffc2abf8abcb0a7d4
OI-liyifan202201 <liyifan202201@outlook.com>
2025-08-23 21:52:16 +0800

    Update start.cmd

--- VScode-Install/start.cmd
+++ VScode-Install/start.cmd
@@ -43,32 +43,34 @@ if %errorlevel% == 2 (
 echo.
 echo 用户选择重新配置，开始清理...
 
-:: 清理 .vscode 目录
+REM 获取系统用户名
+for %%a in ("%userprofile%") do set "YourUsername=%%~nxa"
+echo 正在获取系统用户名: %YourUsername%
+
+REM 清理 .vscode 目录
 set "vscodePath=%userprofile%\.vscode"
 if exist "!vscodePath!" (
-    echo 正在检测 .vscode 文件夹
-    echo 检测到 .vscode 文件夹，开始清理
-    rmdir /s /q "!vscodePath!"
-    echo .vscode 文件夹已清理。
+echo 正在检测 .vscode 文件夹
+echo 检测到 .vscode 文件夹，开始清理
+rmdir /s /q "!vscodePath!"
+echo .vscode 文件夹已清理。
 ) else (
-    echo 未找到 .vscode 文件夹。跳过清理
+echo 未找到 .vscode 文件夹。跳过清理
 )
 
-:: 清理 AppData/Roaming/Code 目录
+REM 清理 AppData/Roaming/Code 目录
 set "codePath=%userprofile%\AppData\Roaming\Code"
 if exist "!codePath!" (
-    echo 正在检测 Code 文件夹
-    echo 检测到 Code 文件夹，开始清理
-    rmdir /s /q "!codePath!"
-    echo Code 文件夹已清理。
+echo 正在检测 Code 文件夹
+echo 检测到 Code 文件夹，开始清理
+rmdir /s /q "!codePath!"
+echo Code 文件夹已清理。
 ) else (
-    echo 未找到 Code 文件夹。跳过清理
+echo 未找到 Code 文件夹。跳过清理
 )
 
-echo.
 echo 清理完成
 
-:: 创建确认文件
 set "outputDir=C:\VSc-cpp"
 set "outputFile=C:\VSc-cpp\install-ok.txt"
 
