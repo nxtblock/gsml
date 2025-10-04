ad240000ae96a675ae2de414e6af2f789a9e3f00
OI-liyifan202201 <liyifan202201@outlook.com>
2025-10-04 21:09:46 +0800

    Update start.cmd

--- About/start.cmd
+++ About/start.cmd
@@ -2,7 +2,7 @@
 chcp 65001
 echo [GenGen ToolBox] 关于
 echo 最新版本：
-echo GenGen ToolBox 1.3.1 （2025/8/28）
+echo GenGen ToolBox 1.4.1 （2025/10/4）
 
 echo GenGen Script Market Launcher in github （2025/8/30）
 echo ---
